a214233ef6fab312a4af953b542d7f76b72a4d0b
yujinghui <jinghuiace@126.com>
2015-11-19 11:55:50 +0800

    init yujinghui

---
+++ blueprint/yujinghui/view.py
@@ -0,0 +1,16 @@
+__author__ = 'yujinghui'
+
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+from flask import Blueprint, render_template, abort
+from flask import Flask, request, g, redirect, url_for
+from jinja2 import TemplateNotFound
+import json, random, datetime, threading
+from sqlalchemy import or_, between
+
+yujinghui = Blueprint('yujinghui', __name__,
+                      template_folder='templates/')
+
+@yujinghui.route('/yujinghui', methods = ['GET','POST'])
+def resume():
+    return render_template("yujinghui.html")
