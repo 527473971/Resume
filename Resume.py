b2ff7318c71d041dc3029041360a7a3ac1623f38
yujinghui <jinghuiace@126.com>
2015-11-19 20:39:34 +0800

    resume starts

--- Resume.py
+++ Resume.py
@@ -1,12 +1,11 @@
 from flask import Flask
-
 from blueprint.yujinghui.view import yujinghui
 from blueprint.zhengyinghao.view import zhengyinghao
 
-app = Flask(__name__, static_folder='static')
+app = Flask(__name__, static_folder='static', static_path='/static')
 
-app.register_blueprint(yujinghui, url_prefix='/yujinghui')
-app.register_blueprint(zhengyinghao, url_prefix='/zhengyinghao')
+app.register_blueprint(yujinghui)
+app.register_blueprint(zhengyinghao)
 
 app.jinja_env.variable_start_string = '{{ '
 
@@ -16,6 +15,9 @@ app.jinja_env.variable_end_string = ' }}'
 def user_interceptor(exception=None):
     print "interceptor end..."
 
+@app.route('/', methods = ['GET',])
+def index():
+    return "this is a homepage about resume"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=9007)
+    app.run(port=9007)
--- blueprint/yujinghui/view.py
+++ blueprint/yujinghui/view.py
@@ -2,17 +2,11 @@ __author__ = 'yujinghui'
 
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
-from flask import Blueprint, render_template, abort
-from flask import Flask, request, g, redirect, url_for
-from jinja2 import TemplateNotFound
-import json, random, datetime, threading
-from sqlalchemy import or_, between
+from flask import Blueprint, render_template
 
 yujinghui = Blueprint('yujinghui', __name__,
                       template_folder='templates/')
 
 @yujinghui.route('/yujinghui', methods = ['GET',])
 def resume():
-    print "ensure route"
-    print "ensure route"
     return render_template("yujinghui.html")
