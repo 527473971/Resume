43d36a631335ad37a03cc9257cc0bffb0ac4e87c
yujinghui <jinghuiace@126.com>
2015-11-19 12:14:05 +0800

    resume starts

--- Resume.py
+++ Resume.py
@@ -1,6 +1,16 @@
 from flask import Flask
 
-app = Flask(__name__)
+from blueprint.yujinghui.view import yujinghui
+from blueprint.zhengyinghao.view import zhengyinghao
+
+app = Flask(__name__, static_folder='static', static_path='/activity/static', template_folder='templates')
+
+app.register_blueprint(yujinghui, url_prefix='/yujinghui')
+app.register_blueprint(zhengyinghao, url_prefix='/zhengyinghao')
+
+app.jinja_env.variable_start_string = '{{ '
+
+app.jinja_env.variable_end_string = ' }}'
 
 if __name__ == '__main__':
     app.run()
